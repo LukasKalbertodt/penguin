23f73c7dcb0fd822e994e1ba6805e374ef0ff308
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-02-20 13:51:53 +0100

    Add `Error` type

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,4 +1,15 @@
+use std::io;
 mod config;
 
 
 pub use config::{Config, ProxyTarget, ProxyTargetError};
+
+#[non_exhaustive]
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("hyper HTTP server error: {0}")]
+    Hyper(#[from] hyper::Error),
+
+    #[error("IO error: {0}")]
+    Io(#[from] io::Error),
+}
