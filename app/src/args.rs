1c33f73bd47aff74fa8c46860ed357368e29f897
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-02-22 23:53:02 +0100

    Add first version of CLI app

---
+++ app/src/args.rs
@@ -0,0 +1,74 @@
+use std::{net::IpAddr, path::{Path, PathBuf}, str::FromStr};
+
+use structopt::StructOpt;
+use penguin::ProxyTarget;
+
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "Penguin",
+    about = "Language-agnostic dev server that can serve directories and act as a proxy.",
+    setting(structopt::clap::AppSettings::VersionlessSubcommands),
+)]
+pub(crate) struct Args {
+    /// The port that the Penguin server listens on.
+    #[structopt(short, long, default_value = "4090")]
+    pub(crate) port: u16,
+
+    /// Address to bind to. Mostly useful to set to "0.0.0.0" to let other
+    /// devices in your network access the server.
+    #[structopt(long, default_value = "127.0.0.1")]
+    pub(crate) bind: IpAddr,
+
+    /// Mount a directory on an URI path: '--mount <uri>:<path>', e.g. '--mount
+    /// assets:/home/peter/images'. Can be specified multiple times. If you only
+    /// want to mount one directory in the root, rather use the `penguin serve`
+    /// subcommand.
+    #[structopt(long = "--mount", number_of_values = 1)]
+    pub(crate) mounts: Vec<Mount>,
+
+    #[structopt(subcommand)]
+    pub(crate) cmd: Command,
+}
+
+#[derive(Debug, StructOpt)]
+pub(crate) enum Command {
+    /// Serve the specified directory as file server.
+    ///
+    /// You can mount more directories via '--mount'. If you don't specify a
+    /// main directory for this subcommand, you have to mount at least one
+    /// directory via '--mount'.
+    Serve {
+        #[structopt(parse(from_os_str))]
+        path: Option<PathBuf>
+    },
+
+    /// Starts a server forwarding all request to the specified target address.
+    Proxy {
+        target: ProxyTarget,
+    },
+}
+
+#[derive(Debug)]
+pub(crate) struct Mount {
+    pub(crate) uri_path: String,
+    pub(crate) fs_path: PathBuf,
+}
+
+impl FromStr for Mount {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let colon_pos = s.find(':').ok_or("does not contain a colon")?;
+        let fs_path = Path::new(&s[colon_pos + 1..]).to_owned();
+
+        let mut uri_path = s[..colon_pos].to_owned();
+        if !uri_path.starts_with('/') {
+            uri_path.insert(0, '/');
+        }
+        if uri_path.ends_with('/') && uri_path.len() > 1 {
+            uri_path.pop();
+        }
+
+        Ok(Self { uri_path, fs_path})
+    }
+}
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,1 +1,43 @@
-fn main() {}
+use penguin::Config;
+use structopt::StructOpt;
+
+use crate::args::{Args, Command};
+
+mod args;
+
+
+// A single thread runtime is plenty enough for a webserver purpose.
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Parse CLI arguments.
+    let args = Args::from_args();
+
+    // Build Penguin configuration from arguments.
+    let bind_addr = (args.bind, args.port).into();
+    let mut config = Config::new(bind_addr);
+    match args.cmd {
+        Command::Proxy { target } => config = config.proxy(target),
+        Command::Serve { path } => {
+            if let Some(path) = path {
+                config = config.add_serve_dir("/", &path);
+            } else if args.mounts.is_empty() {
+                bunt::eprintln!(
+                    "{$red+bold}error:{/$} neither serve path nor '--mount' arguments \
+                        given, but at least one path has to be specified!"
+                );
+                std::process::exit(1);
+            }
+
+            for mount in &args.mounts {
+                config = config.add_serve_dir(&mount.uri_path, &mount.fs_path);
+            }
+        },
+    }
+
+    let (_controller, serve) = penguin::serve(config)?;
+
+    bunt::println!("Penguin 🐧 is listening on {$yellow+intense+bold}http://{}{/$}", bind_addr);
+    serve.await?;
+
+    Ok(())
+}
